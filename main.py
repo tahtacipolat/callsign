621dc366077d2e82c0e136e1c4cf61604bf8418b
tahtacipolat <tahtacipolat@gmail.com>
2022-06-20 19:26:54 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,75 @@
+kelime = input("kelimeyi giriniz")
+print(kelime)
+for harf in kelime:
+    if harf == "a":
+        print("alfa")
+    if harf == "b":
+        print("bravo")
+    if harf == "c":
+        print("charlie")
+    if harf == "d":
+        print("delta")
+    if harf == "e":
+        print("echo")
+    if harf == "f":
+        print("foxtrot")
+    if harf == "g":
+        print("golf")
+    if harf == "h":
+        print("hotel")
+    if harf == "i":
+        print("india")
+    if harf == "j":
+        print("juliet")
+    if harf == "k":
+        print("kilo")
+    if harf == "l":
+        print("lima")
+    if harf == "m":
+        print("mike")
+    if harf == "n":
+        print("november")
+    if harf == "o":
+        print("oscar")
+    if harf == "p":
+        print("papa")
+    if harf == "q":
+        print("quebec")
+    if harf == "r":
+        print("romeo")
+    if harf == "s":
+        print("sierra")
+    if harf == "t":
+        print("tango")
+    if harf == "u":
+        print("uniform")
+    if harf == "v":
+        print("victor")
+    if harf == "w":
+        print("whiskey")
+    if harf == "x":
+        print("x-ray")
+    if harf == "y":
+        print("yankee")
+    if harf == "z":
+        print("zulu")
+    if harf == "1":
+        print("one")
+    if harf == "2":
+        print("two")
+    if harf == "3":
+        print("three")
+    if harf == "4":
+        print("four")
+    if harf == "5":
+        print("five")
+    if harf == "6":
+        print("six")
+    if harf == "7":
+        print("seven")
+    if harf == "8":
+        print("eight")
+    if harf == "9":
+        print("nine")
+    if harf == "0":
+        print("zero")
